e2211042ef012d49869b5d9a3e2a27ae1f87c1f2
Oscar Delicado <oscardelicadohernandez@gmail.com>
2024-03-06 11:50:53 +0100

    Added gitignore file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-credentials = Credentials.from_service_account_file("/.gitignore/.venv/credentials.json", scopes=scopes)
+credentials = Credentials.from_service_account_file("credentials.json", scopes=scopes)
 client = gspread.authorize(credentials=credentials)
 
 sheet_id = "1NSfKKmZMTfxyObacflA8OfjBPA66OrSuyfi_Ox0v1Go"
+sheet = client.open_by_key(key=sheet_id)
